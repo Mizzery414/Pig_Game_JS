eb9705501e7ac0bea8544d93b34978351eeb5f9d
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-12-05 20:40:35 -0800

    took off rules button, change css, end game

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ diceEl.classList.add("hidden");
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 // Must be outside the function, otherwise will reset every time we click roll dice function
 
 // Rolling dice functionality
@@ -31,52 +40,37 @@ btnRoll.addEventListener("click", function () {
   // 2. Display dice roll
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
 
   // 3. Check for a roll of 1
   if (dice !== 1) {
     // 3b. Add dice to current score
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
+    document.getElementById(`score--${activePlayer}`).textContent =
       currentScore;
   } else {
+    switchPlayer();
     // 4. If true, switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
   }
 });
 
-// Rules Button to show/hide rules on Click
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".show-modal");
+btnHold.addEventListener("click", function () {
+  // 1. Add current score to active player score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", openModal);
-}
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  console.log(e.key);
-
-  //if pressed key is "escape" AND not-modal contains class "hidden" - close the modal
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
+  // 2. Check score is >= 100
+  if (scores[activePlayer] >= 20) {
+    // 3. Finish the game
+    playing = false;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    // 4. Switch to the next player
+    switchPlayer();
   }
 });
