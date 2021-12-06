57dbe31614f19340079e977a105dd6f85ca2614b
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-12-06 13:50:53 -0800

    create new function for game reset - midpoint

--- script.js
+++ script.js
@@ -14,14 +14,24 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -79,3 +89,12 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+//  My Attempt :
+// function reload() {
+//   document.getElementById("btnNew").addEventListener("click", scores);
+//   score0El.location.reload();
+//   score1El.location.reload();
+// }
+
+btnNew.addEventListener("click", function () {});
