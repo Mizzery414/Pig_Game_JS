0e63f37b7569a4d92c5883ba86ca22ab5fd7946f
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-12-06 13:57:52 -0800

    new game button complete

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
 
 const init = function () {
-  const scores = [0, 0];
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
   score0El.textContent = 0;
   score1El.textContent = 0;
@@ -32,6 +34,8 @@ const init = function () {
   player0El.classList.add("player--active");
   player1El.classList.remove("player--active");
 };
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -71,7 +75,6 @@ btnHold.addEventListener("click", function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-
     // 2. Check score is >= 100
     if (scores[activePlayer] >= 20) {
       // 3. Finish the game
@@ -97,4 +100,4 @@ btnHold.addEventListener("click", function () {
 //   score1El.location.reload();
 // }
 
-btnNew.addEventListener("click", function () {});
+btnNew.addEventListener("click", init);
