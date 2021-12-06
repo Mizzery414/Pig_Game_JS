44dc00f3b619989d72be9506f7d945515345443a
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-12-06 15:01:00 -0800

    helps when you don't double your js code...

--- script.js
+++ script.js
@@ -101,117 +101,16 @@ btnHold.addEventListener("click", function () {
 // }
 
 btnNew.addEventListener("click", init);
-// Selecting elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
-
-const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
-
-let scores, currentScore, activePlayer, playing;
-
-// Starting conditions
-
-const init = function () {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  diceEl.classList.add("hidden");
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-};
-
-init();
-
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
-};
-// Must be outside the function, otherwise will reset every time we click roll dice function
-
-// Rolling dice functionality
-btnRoll.addEventListener("click", function () {
-  if (playing) {
-    // 1. Generate random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display dice roll
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
-
-    // 3. Check for a roll of 1
-    if (dice !== 1) {
-      // 3b. Add dice to current score
-      currentScore += dice;
-      document.getElementById(`score--${activePlayer}`).textContent =
-        currentScore;
-    } else {
-      switchPlayer();
-      // 4. If true, switch to next player
-    }
-  }
-});
-
-btnHold.addEventListener("click", function () {
-  if (playing) {
-    // 1. Add current score to active player score
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
-    // 2. Check score is >= 100
-    if (scores[activePlayer] >= 20) {
-      // 3. Finish the game
-      playing = false;
-      diceEl.classList.add("hidden");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
-    } else {
-      // 4. Switch to the next player
-      switchPlayer();
-    }
-  }
-});
-
-//  My Attempt : ish. overthinking it again.
-// function reload() {
-//   document.getElementById("btnNew").addEventListener("click", scores);
-//   score0El.location.reload();
-//   score1El.location.reload();
-// }
-
-btnNew.addEventListener("click", init);
 
 // MODAL RULES BOX
 // Get the modal
-const modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
